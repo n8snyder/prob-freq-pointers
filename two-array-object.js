c47dd1a2bee9b2e3f6dbd52e7538c4f8a4380730
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-14 16:59:26 -0800

    Implemented two array object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,18 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+"use strict";
+/** Creates object from array of keys and array of values
+ * If not enough values, the extra keys have value null.
+ * If not enough keys, the extra values are ignored.
+ */
+function twoArrayObject(keys, values) {
+    const obj = {}
+
+    for (let i = 0; i < keys.length; i++) {
+        if (i >= values.length) {
+            obj[keys[i]] = null;
+        } else {
+            obj[keys[i]] = values[i]
+        }
+    }
+
+    return obj;
 }
