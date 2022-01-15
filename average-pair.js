81f723cd48467b1c9ab8763a84c77b393abbfe4e
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-14 17:00:15 -0800

    Implemented average pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,18 @@
-// add whatever parameters you deem necessary
-function averagePair() {
+"use strict";
+
+/** Determines if a pair of numbers exist which average to the target number */
+function averagePair(nums, target) {
+    if (!nums) return false;
+
+    const numLookup = new Set()
+    for (let n of nums) {
+        numLookup.add(n);
+    }
+
+    for (let n of nums) {
+        if (numLookup.has(target * 2 - n)) {
+            return true;
+        }
+    }
+    return false;
 }
